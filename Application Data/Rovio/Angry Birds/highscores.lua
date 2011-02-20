026aabe6f8d7618464d6776d868f8699ee0f04aa
U-hjs-2011JanNb\hjs <github@hjs.twbbs.org>
2011-02-20 13:03:02 +0800

    finish level 1
    Signed-off-by: U-hjs-2011JanNb\hjs <github@hjs.twbbs.org>

--- Application Data/Rovio/Angry Birds/highscores.lua
+++ Application Data/Rovio/Angry Birds/highscores.lua
@@ -5,6 +5,13 @@ Level57 = {
     lowScore = 43220,
 
 }
+LevelGE_2 = {
+    completed = false,
+    birds = 0,
+    score = 0,
+    lowScore = 0,
+
+}
 Level17 = {
     completed = true,
     birds = 3,
@@ -54,6 +61,13 @@ Level4 = {
     lowScore = 37070,
 
 }
+Level14 = {
+    completed = true,
+    birds = 5,
+    score = 36100,
+    lowScore = 36100,
+
+}
 Level15 = {
     completed = true,
     birds = 5,
@@ -75,6 +89,13 @@ Level13 = {
     lowScore = 36420,
 
 }
+Level42 = {
+    completed = true,
+    birds = 4,
+    score = 103960,
+    lowScore = 103960,
+
+}
 Level53 = {
     completed = true,
     birds = 1,
@@ -82,11 +103,46 @@ Level53 = {
     lowScore = 42440,
 
 }
+Level34 = {
+    completed = true,
+    birds = 3,
+    score = 59890,
+    lowScore = 59890,
+
+}
+Level16 = {
+    completed = true,
+    birds = 3,
+    score = 30190,
+    lowScore = 30190,
+
+}
 Level6 = {
     completed = true,
     birds = 1,
     score = 67850,
     lowScore = 67850,
+
+}
+Level52 = {
+    completed = true,
+    birds = 4,
+    score = 47190,
+    lowScore = 47190,
+
+}
+Level44 = {
+    completed = true,
+    birds = 7,
+    score = 69170,
+    lowScore = 69170,
+
+}
+Level23 = {
+    completed = true,
+    birds = 4,
+    score = 42860,
+    lowScore = 42860,
 
 }
 Level3 = {
@@ -103,17 +159,17 @@ Level10 = {
     lowScore = 18460,
 
 }
-Level12 = {
-    completed = true,
-    birds = 3,
-    score = 36470,
-    lowScore = 36470,
-
-}
 Level39 = {
     completed = true,
     birds = 3,
     score = 62300,
     lowScore = 62300,
+
+}
+Level12 = {
+    completed = true,
+    birds = 3,
+    score = 36470,
+    lowScore = 36470,
 
 }
