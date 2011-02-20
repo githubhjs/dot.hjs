8da6949df22d739ba2a156864bb84422c38e378f
U-HJS-NOV10-F0EEE\S.XP <github@hjs.twbbs.org>
2011-02-20 12:33:50 +0800

    Angry Bird PC saveas
    Signed-off-by: U-HJS-NOV10-F0EEE\S.XP <github@hjs.twbbs.org>

---
+++ Application Data/Rovio/Angry Birds/highscores.lua
@@ -0,0 +1,119 @@
+Level57 = {
+    completed = true,
+    birds = 3,
+    score = 43220,
+    lowScore = 43220,
+
+}
+Level17 = {
+    completed = true,
+    birds = 3,
+    score = 36160,
+    lowScore = 36160,
+
+}
+Level1 = {
+    completed = true,
+    birds = 1,
+    score = 33670,
+    lowScore = 10720,
+
+}
+Level9 = {
+    completed = true,
+    birds = 2,
+    score = 46510,
+    lowScore = 46510,
+
+}
+Level2 = {
+    completed = true,
+    birds = 3,
+    score = 25780,
+    lowScore = 25780,
+
+}
+Level7 = {
+    completed = true,
+    birds = 4,
+    score = 25140,
+    lowScore = 25140,
+
+}
+Level5 = {
+    completed = true,
+    birds = 3,
+    score = 34870,
+    lowScore = 34870,
+
+}
+Level4 = {
+    completed = true,
+    birds = 2,
+    score = 37070,
+    lowScore = 37070,
+
+}
+Level15 = {
+    completed = true,
+    birds = 5,
+    score = 38600,
+    lowScore = 38600,
+
+}
+Level8 = {
+    completed = true,
+    birds = 3,
+    score = 58620,
+    lowScore = 58620,
+
+}
+Level13 = {
+    completed = true,
+    birds = 4,
+    score = 36420,
+    lowScore = 36420,
+
+}
+Level53 = {
+    completed = true,
+    birds = 1,
+    score = 42440,
+    lowScore = 42440,
+
+}
+Level6 = {
+    completed = true,
+    birds = 1,
+    score = 67850,
+    lowScore = 67850,
+
+}
+Level3 = {
+    completed = true,
+    birds = 4,
+    score = 11120,
+    lowScore = 11120,
+
+}
+Level10 = {
+    completed = true,
+    birds = 4,
+    score = 18460,
+    lowScore = 18460,
+
+}
+Level12 = {
+    completed = true,
+    birds = 3,
+    score = 36470,
+    lowScore = 36470,
+
+}
+Level39 = {
+    completed = true,
+    birds = 3,
+    score = 62300,
+    lowScore = 62300,
+
+}
---
+++ Application Data/Rovio/Angry Birds/settings.lua
@@ -0,0 +1,55 @@
+lastOpenLevelLP3 = 1
+openGoldenEggLevels = {
+
+}
+lastOpenLevelLP4 = 1
+lastOpenLevelLP2 = 1
+audioEnabled = true
+bullsEye = false
+selectedEpisode = 1
+cumulativeStars = 32
+playtime = 1827.35
+gameStarted = true
+cumulativeScore = "684740"
+birdsShooted = 138
+currentZoomLevelMainMenu = 1.83333
+wilhelmTell = false
+tutorials = {
+    BIRD_YELLOW = {
+        sprite = "TUTORIAL_3",
+
+    },
+    BIRD_BLUE = {
+        sprite = "TUTORIAL_2",
+
+    },
+    BIRD_RED = {
+        sprite = "TUTORIAL_1",
+
+    },
+
+}
+eaglesUsedIn = {
+
+}
+currentMainMenuTheme = "theme1"
+totalLevelsCompleted = 19
+eaglesAvailable = {
+    extra = 1,
+    pack4 = 1,
+    pack3 = 1,
+    basic = 1,
+
+}
+mightyEagleEnabled = false
+gameRestarted = 4
+currentLevelSelectionPages = {
+    pack4 = 1,
+    basic = 1,
+    pack3 = 1,
+    goldeneggs = 1,
+    extra = 1,
+
+}
+lastOpenLevel = 18
+gameStarts = 2
